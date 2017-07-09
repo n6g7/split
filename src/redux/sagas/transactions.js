0bd2d0f6a041fe5d07a162c12248a7ed16730268
Nathan Gaberel <nathang@theodo.co.uk>
2017-07-09 14:03:21 +0100

    Sort transactions.

--- src/redux/sagas/transactions.js
+++ src/redux/sagas/transactions.js
@@ -7,13 +7,19 @@ import { syncTransactions } from '@actions/transactions'
 import { types } from '@actions/user'
 
 const transactionsTransformer = obj =>
-  Object.keys(obj).map(key => ({
+  Object.keys(obj)
+  .map(key => ({
     ...obj[key],
     created: moment(obj[key].created),
     key,
     settled: moment(obj[key].settled),
     updated: moment(obj[key].updated)
   }))
+  .sort((a, b) => {
+    if (a.created.isBefore(b.created)) return 1
+    else if (a.created.isAfter(b.created)) return -1
+    else return 0
+  })
 
 function * syncTransactionsSaga () {
   const uid = yield select(state => state.user.user.uid)
