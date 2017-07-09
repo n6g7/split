c98931b2b3cc9225dba5d7fbde55ec554ee0e376
Nathan Gaberel <nathang@theodo.co.uk>
2017-07-09 13:20:03 +0100

    Use cool prop types.

--- src/page.js
+++ src/page.js
@@ -1,9 +1,14 @@
 import React, { PureComponent } from 'react'
+import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 
 import { login } from '@actions/user'
 
 class Page extends PureComponent {
+  static propTypes = {
+    login: PropTypes.func.isRequired
+  }
+
   render () {
     return <button onClick={this.props.login}>Login</button>
   }
